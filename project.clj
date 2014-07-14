60b8d2d1b956e688c55999f2f006733066f4f853
Perttu Auramo <perttu.auramo@gmail.com>
2014-07-14 23:55:42 +0300

    min lein version added

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject holston "0.1.0-SNAPSHOT"
   :description "Beer rating system"
+  :min-lein-version "2.0.0"
   :url "http://holston.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
