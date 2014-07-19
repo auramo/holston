d144f450f0090421cc7c0827e13fde374893072f
Perttu Auramo <perttu.auramo@gmail.com>
2014-07-19 12:06:18 +0300

    raw data to map

--- src/holston/dataimport.clj
+++ src/holston/dataimport.clj
@@ -1,8 +1,15 @@
 (ns holston.dataimport
-  (:require [clojure-csv.core :as csv]))
+  (:require [clojure-csv.core :as csv])
+  (:use [clojure.java.io :only [reader]]))
 
 (defn take-csv
   "Takes file name and reads data."
-  [fname]
-  (with-open [file (clojure.java.io/reader fname)]
+  [file-name]
+  (with-open [file (reader file-name)]
     (csv/parse-csv (slurp file))))
+
+(defn parse-row [row]
+  {:name (get row 4) :brewery (get row 5)})
+
+(defn parse-tastings [file-name]
+    (map parse-row (take-csv file-name)))
