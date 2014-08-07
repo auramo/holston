aa60a4a35fcb50497f28315700fdd6c860cc4be4
Perttu Auramo <perttu.auramo@gmail.com>
2014-08-07 18:48:56 +0300

    Authentication works
    got email now
    
    remove the profile request
    
    email address retrieved
    
    dummy check for permissions

--- project.clj
+++ project.clj
@@ -5,12 +5,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.apache.httpcomponents/httpclient "4.3.3"]
                  [http-kit "2.1.16"]
                  [javax.servlet/servlet-api "2.5"]
                  [compojure "1.1.8"]
                  [org.clojure/java.jdbc "0.3.4"]
                  [postgresql "9.1-901.jdbc4"]
-                 [clojure-csv/clojure-csv "2.0.1"]] ;;https://github.com/davidsantiago/clojure-csv
+                 [clojure-csv/clojure-csv "2.0.1"]  ;;https://github.com/davidsantiago/clojure-csv
+                 [com.cemerick/friend "0.2.0" :exclusions [ring/ring-core]]
+                 [friend-oauth2 "0.1.1" :exclusions [org.apache.httpcomponents/httpcore]]
+                 [cheshire "5.2.0"]
+                 [ring-server "0.3.0" :exclusions [ring]]
+                 [clj-http "0.9.2"]]
 ;; uberjar doesn't seem to work now...
   :uberjar-name "holston-standalone.jar"
   :main ^:skip-aot holston.core
--- src/holston/core.clj
+++ src/holston/core.clj
@@ -1,25 +1,92 @@
 (ns holston.core
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [cemerick.friend :as friend]
+            [friend-oauth2.workflow :as oauth2]
+            [friend-oauth2.util :refer [format-config-uri]]
+            [cheshire.core :as j]
+            [clj-http.client :as client]
+            (cemerick.friend [workflows :as workflows]
+                             [credentials :as creds]))
   (:use [compojure.route :only [files not-found]]
-      [compojure.handler :only [site]]
-      [compojure.core :only [defroutes GET POST DELETE ANY context]]
-      [clojure.java.jdbc :as sql]
-      org.httpkit.server)
+        [compojure.handler :only [site]]
+        [compojure.core :only [defroutes GET POST DELETE ANY context]]
+        [clojure.java.jdbc :as sql]
+        org.httpkit.server)
   (:gen-class))
 
-(def db-url (or (System/getenv "DATABASE_URL")
-              "postgresql://localhost:5432/holston"))
+(def config-auth {:roles #{::user}})
 
-(defn show-landing-page [req]
-  (println (str "DB url:" db-url))
-  (str "Holston is ready for e-business " (:?column? (first (sql/query db-url
-           ["select 'with a DB!'"])))))
+(def client-config
+  {:client-id "641379821393-vpt0pi6bdgntd751l2bd5hm1bf7vqrl7.apps.googleusercontent.com"
+   :client-secret "f9mMWwhsqhkY9_N1OthLenKO"
+   :callback {:domain "http://localhost:8080" :path "/oauth2callback"}})
 
-(defroutes all-routes
-  (GET "/" [] show-landing-page)
-  (files "/static/") ;; static file url prefix /static, in `public` folder
-  (not-found "<p>Page not found.</p>"))
+(def uri-config
+  {:authentication-uri {:url "https://accounts.google.com/o/oauth2/auth"
+                       :query {:client_id (:client-id client-config)
+                               :response_type "code"
+                               :redirect_uri (format-config-uri client-config)
+                               :scope "https://www.googleapis.com/auth/userinfo.email" }}
+
+   :access-token-uri {:url "https://accounts.google.com/o/oauth2/token"
+                      :query {:client_id (:client-id client-config)
+                              :client_secret (:client-secret client-config)
+                              :grant_type "authorization_code"
+                              :redirect_uri (format-config-uri client-config)}}})
+
+(defn- fetch-email-address [access-token]
+  (let [response (client/get
+                  "https://www.googleapis.com/oauth2/v2/userinfo"
+                  {:as :json
+                   :headers {"Authorization" (str "Bearer " access-token)}})]
+  (if (= 200 (:status response))
+    (:email (:body response))
+    (throw (Exception. "Couldn't retrieve email address")))))
+
+;; Dummy for now
+(defn- fetch-roles [email]
+  (if (= email "foo.bar@gmail.com")
+    #{::admin ::user}
+    #{::user}))
+
+(defroutes ring-app
+  (GET "/" request "open.")
+  (GET "/status" request
+       (let [count (:count (:session request) 0)
+             session (assoc (:session request) :count (inc count))]
+         (-> (ring.util.response/response
+              (str "<p>We've hit the session page " (:count session)
+                   " times.</p><p>The current session: " session "</p>"))
+             (assoc :session session))))
+  (GET "/authlink" request
+       (friend/authorize #{::user} "Authorized page."))
+  (GET "/authlink2" request
+       (friend/authorize #{::user} "Authorized page 2."))
+  (GET "/admin" request
+       (friend/authorize #{::admin} "Only admins can see this page."))
+  (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
+  (files "/static/"))
+
+(def app
+  (handler/site
+   (friend/authenticate
+    ring-app
+    {:allow-anon? true
+     :workflows [(oauth2/workflow
+                  {:client-config client-config
+                   :uri-config uri-config
+                   ;;:config-auth config-auth
+                   :auth-error-fn (fn [error]
+                                    (ring.util.response/response error))
+                   :credential-fn (fn [token]
+                                    (let [email (fetch-email-address (:access-token token))]
+                                      {:identity token
+                                       :email email
+                                       :roles (fetch-roles email)}))})]})))
 
 (defn -main
   "Starts the server"
   [port]
-  (run-server (site #'all-routes) {:port (Integer. port)}))
+  (run-server (site #'app) {:port (Integer. port)}))
