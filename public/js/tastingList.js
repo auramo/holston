babf9a16ea9787c29a5053137cea97a0237f6a75
Perttu Auramo <perttu.auramo@gmail.com>
2014-10-19 18:30:57 +0300

    Implement basic filtering stuff

--- public/js/tastingList.js
+++ public/js/tastingList.js
@@ -1,6 +1,10 @@
 angular.module('tastingList', []).controller('TastingListController', ['$scope', '$http',
     function($scope, $http) {
         $scope.tastingCount = 0
+        $scope.tastings = [
+            {'beer': 'Ale Kokki', 'brewery': 'Joku virolaine', 'averageRating': 44},
+            {'beer': 'Saku', 'brewery': 'Saku varmaa', 'averageRating': 22}
+        ]
         $http.get('/api/tastings/count').
             success(function(data, status, headers, config) {
                 $scope.tastingCount = data.count
