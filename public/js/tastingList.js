32713e397a4b9d654076879e184e0c1df2883c82
Perttu Auramo <perttu.auramo@gmail.com>
2014-10-19 18:14:44 +0300

    Use real count

--- public/js/tastingList.js
+++ public/js/tastingList.js
@@ -1,5 +1,12 @@
-angular.module('tastingList', []).controller('TastingListController', ['$scope',
-    function($scope) {
-        $scope.tastingCount = 5
+angular.module('tastingList', []).controller('TastingListController', ['$scope', '$http',
+    function($scope, $http) {
+        $scope.tastingCount = 0
+        $http.get('/api/tastings/count').
+            success(function(data, status, headers, config) {
+                $scope.tastingCount = data.count
+            }).
+            error(function(data, status, headers, config) {
+                console.log('error getting count', status, data)
+            })
     }]
 );
